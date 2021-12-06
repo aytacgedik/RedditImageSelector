d9e236253b380718ee0a27b6cae2807f55d98f45
Aytac Gedik <aytacgedik20@gmail.com>
2021-12-06 02:51:38 +0100

    3-Implementation-of-Controller

---
+++ Controllers/ImageDownloadController.cs
@@ -0,0 +1,83 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+using RedditImgDownloader.Repositories;
+
+namespace RedditImgDownloader.Controllers
+{
+    ///<summary>
+    /// The <c>ImageDownloadController</c> provides two method to
+    /// first method selects random image from given repository and saves it's select date,
+    /// second method returns all selected images and its selected date as json
+    ///</summary>
+    [ApiController]
+    [Route("")]
+    public class ImageDownloadController : ControllerBase
+    {
+        private readonly IDownloadRepository repository;
+
+        ///<summary>
+        /// Initializes a new instance of a
+        /// <c>ImageDownloadController</c> type
+        ///</summary>
+        /// <param name="repository">
+        ///   A <see cref="IDownloadRepository"/> type representing a value.
+        /// </param>
+        public ImageDownloadController(IDownloadRepository repository) //Inject the repository dependency
+        {
+            this.repository = repository;
+        }
+
+        ///<summary>
+        /// Returns selected images and their select dates.
+        /// In case of error, it returns error message.
+        ///</summary>
+        /// <exception cref="Exception">
+        ///   if repository.ReturnJsonFiles method throws an exception.
+        /// </exception>
+        /// <returns>A <see cref="ContentResult" or A <see cref="NotFoundObjectResult"/></returns>
+        [HttpGet]
+        [Route("history")]
+        public ActionResult ReturnJsonFiles()
+        {
+            try
+            {
+                var jsonFiles = repository.ReturnJsonFiles();
+                return Content(jsonFiles, "application/json");
+            }
+            catch (Exception e)
+            {
+                return NotFound(e.Message);
+            }
+        }
+
+        ///<summary>
+        /// Takes subreddit as argument and saves image and its select date.
+        /// In case of error, it returns error message.
+        ///</summary>
+        /// <param name="subreddit">
+        ///   A <see cref="string"/> type representing a value.
+        /// </param>
+        ///</summary>
+        /// <exception cref="Exception">
+        ///   if repository.CreateJsonFile method throws an exception.
+        /// </exception>
+        /// <returns>A <see cref="CreatedAtActionResult" or A <see cref="NotFoundObjectResult"/></returns>
+        [HttpPost]
+        [Route("random")]
+        public ActionResult CreateJsonFile(string subreddit)
+        {
+            try
+            {
+                repository.CreateJsonFile(subreddit);
+                return CreatedAtAction(nameof(ReturnJsonFiles), subreddit);
+            }
+            catch (Exception e)
+            {
+                return NotFound(e.Message);
+            }
+
+        }
+
+
+    }
+}
